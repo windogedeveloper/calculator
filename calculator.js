ef937ca255e96009fce6dac959c55eb140babe24
Parth Kasmalkar <parth.kasmalkar@warriorlife.net>
2017-12-15 15:02:22 -0800

    Version 1.0.6
    Created logic for negative function

--- calculator.js
+++ calculator.js
@@ -13,12 +13,44 @@ function v(val) {
 //Evaluate the equation
 function e() {
 		try {
-			d(eval(document.getElementById("d").value));
+			 d(eval(document.getElementById("d").value));
 		}
 		catch(err) {
 				d("Error");
 		}
 }
-function negative(val) {
-		
+function negative() {
+	// evaluate the current equation
+	e();
+	// get teh value on the display and multiply it by -1
+	var x = Number(document.getElementById("d").value);
+	alert(x);
+	var y = x*(-1);
+	alert(y);
+	// display the result
+	d(y);
+}
+function absolute() {
+	// evaluate the current equation
+	// get teh value on the display and multiply it by -1
+	var x = Number(e());
+	var y = x*(-1);
+	// display the result
+	d(y);
+}
+function exponent() {
+	// evaluate the current equation
+	// get teh value on the display and multiply it by -1
+	var x = Number(e());
+	var y = x*(-1);
+	// display the result
+	d(y);
+}
+function squareroot() {
+	// evaluate the current equation
+	// get teh value on the display and multiply it by -1
+	var x = Number(e());
+	var y = x*(-1);
+	// display the result
+	d(y);
 }
